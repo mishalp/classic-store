9afb5e967708ace5890064c786843690853d7b2b
mishalp <mail4mishal@gmail.com>
2024-07-03 10:18:01 +0530

    Hamburger menu added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,17 @@ import React from 'react'
 import Link from 'next/link'
 import { buttonVariants } from './ui/button'
 import LogoutBtn from './LogoutBtn'
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { Menu } from 'lucide-react'
+
 
 
 export default async function Navbar() {
@@ -14,12 +25,12 @@ export default async function Navbar() {
             <Link href="/">
                 <h2 className='text-2xl text-white font-bold'>Classic store</h2>
             </Link>
-            <div className="flex gap-4">
+            <div className="md:flex gap-4 hidden">
                 <Link href="/" className='text-white font-semibold' >Home</Link>
                 <Link href="/products" className='text-white font-semibold' >Products</Link>
                 <Link href="/categories" className='text-white font-semibold' >Categories</Link>
             </div>
-            <div className="flex gap-4 items-center justify-center">
+            <div className="hidden md:flex gap-4 items-center justify-center">
                 {user ? (
                     <>
                         <div className="flex items-center gap-2">
@@ -31,11 +42,56 @@ export default async function Navbar() {
                         <LogoutBtn />
                     </>
 
-                ) : <Link className={buttonVariants({variant: "secondary"})} href="/login">
+                ) : <Link className={buttonVariants({ variant: "secondary" })} href="/login">
                     Login
                 </Link>
                 }
             </div>
+            <div className="md:hidden my-auto">
+                <Sheet>
+                    <SheetTrigger>
+                        <Menu color='#fff' size={32} />
+                    </SheetTrigger>
+                    <SheetContent className='bg-[#1A1B23] border-none'>
+                        <SheetHeader>
+                            <div className="flex flex-col gap-4">
+                                <div className="flex flex-col gap-4 items-center justify-center">
+                                    {user ? (
+                                        <>
+                                            <div className="flex items-center gap-2">
+                                                <div className="size-10 font-bold bg-[#eee] select-none flex items-center justify-center rounded-full ring-1">
+                                                    {user.name.split("")[0]}
+                                                </div>
+                                                <p className='text-white'>{user.name}</p>
+                                            </div>
+                                            <LogoutBtn />
+                                        </>
+
+                                    ) :
+                                        <SheetClose asChild>
+                                            <Link className={buttonVariants({ variant: "secondary" })} href="/login">
+                                                Login
+                                            </Link>
+                                        </SheetClose>
+                                    }
+                                </div>
+                                <div className="flex flex-col items-center mt-10 gap-4">
+                                    <SheetClose asChild>
+                                        <Link href="/" className='text-white text-xl font-semibold' >Home</Link>
+                                    </SheetClose>
+                                    <SheetClose asChild>
+                                        <Link href="/products" className='text-white text-xl font-semibold' >Products</Link>
+                                    </SheetClose>
+                                    <SheetClose asChild>
+                                        <Link href="/categories" className='text-white text-xl font-semibold' >Categories</Link>
+                                    </SheetClose>
+                                </div>
+                            </div>
+                        </SheetHeader>
+                    </SheetContent>
+                </Sheet>
+            </div>
+
         </div>
     )
 }
